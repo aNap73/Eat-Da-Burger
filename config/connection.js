82c6e9a1586a60e6b33e14a141d39592b7e78950
aNap73 <napolitano.anthony@yahoo.com>
2018-05-04 15:47:11 -0400

    Heroku db deploy change ID to id ugh

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 
   var mysql = require("mysql");
-  var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: process.env.sqlpwd,
-    database: "burger_db"
-  });
+  var connection;
+  
+  if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  }else{
+      connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: process.env.sqlpwd,
+      database: "burger_db"});
+  }
   connection.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
@@ -14,7 +19,6 @@
     console.log("connected as id " + connection.threadId);
     
   });
-
   module.exports = connection;
    
 
