442b9d9c8a616fb33d1af49a18825f608e2ad640
aNap73 <napolitano.anthony@yahoo.com>
2018-05-04 16:04:21 -0400

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,16 @@
 
+  require('dotenv').config();
   var mysql = require("mysql");
   var connection;
   
   if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
   }else{
-      // connection = mysql.createConnection({
-      // host: "localhost",
-      // user: "root",
-      // password: process.env.sqlpwd,
-      // database: "burger_db"});
+      connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: process.env.sqlpwd,
+      database: "burger_db"});
   }
   connection.connect(function(err) {
     if (err) {
