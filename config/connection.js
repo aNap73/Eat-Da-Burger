2f35e350859f1adbed6b8ac2ebacded6e2cf7e7c
aNap73 <napolitano.anthony@yahoo.com>
2018-05-04 15:56:49 -0400

    removed local dotenv

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@
   if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
   }else{
-      connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: process.env.sqlpwd,
-      database: "burger_db"});
+      // connection = mysql.createConnection({
+      // host: "localhost",
+      // user: "root",
+      // password: process.env.sqlpwd,
+      // database: "burger_db"});
   }
   connection.connect(function(err) {
     if (err) {
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//require('dotenv').config();
 var express = require("express");
 var bodyParser = require("body-parser");
 
@@ -19,7 +20,7 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-require('dotenv').config();
+
 
 var mod = require('./models/burgers');
 //mod.insertaburger();
