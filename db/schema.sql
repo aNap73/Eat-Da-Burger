09fc35a594bfcaa3d4a9d03e4fb05bdf5d038a66
aNap73 <napolitano.anthony@yahoo.com>
2018-05-07 07:28:23 -0400

    worked on design

--- db/schema.sql
+++ db/schema.sql
@@ -10,10 +10,17 @@ CREATE DATABASE IF NOT EXISTS BURGER_DB;
 
 USE BURGER_DB;
 
-CREATE TABLE BURGERS (
-`ID` int(10) auto_increment NOT NULL,
-`BURGER_NAME` VARCHAR(100) NOT NULL,
-`DEVOURED` BOOL,
-PRIMARY KEY(ID)
-);
+DROP TABLE `burgers`;
 
+CREATE TABLE `burgers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `BURGER_NAME` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `WINNER_NAME` varchar(100) COLLATE utf8_unicode_ci NULL,
+  `DEVOURED` tinyint(1) DEFAULT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DELETE from burgers;
+
+Select * from burgers;
